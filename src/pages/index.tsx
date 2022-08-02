ad548c1494e1d487d6a9beebbb8f37d4086434ba
CesarHenry <cesar_rasec89@hotmail.com>
2022-08-02 18:57:25 -0300

    feat: update project and create page login

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main></main>
+    </>
+  )
+}
+
+export default Home
---
+++ src/styles/theme.ts
@@ -0,0 +1,46 @@
+const theme = {
+  colors: {
+    GRADIENT: 'linear-gradient(to right, #AE45FA 10%, #4B1AC8, #4178D7)'
+  },
+  border: {
+    radius: {
+      SHARP: 0,
+      ROUND: '50%'
+    }
+  },
+  media: {
+    // BREAKPOINTS
+    XS: 0,
+    SM: '36rem',
+    MD: '48rem',
+    LG: '62rem',
+    XL: '75rem',
+    XXL: '87.5rem',
+    X1600: '100rem'
+  },
+  font: {
+    family: {
+      MAIN: 'Poppins, sans-serif',
+      SECONDARY: `'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`
+    },
+    weight: {
+      LIGHT: 300,
+      REGULAR: 400,
+      MEDIUM: 500,
+      SEMI_BOLD: 600,
+      BOLD: 700,
+      EXTRA_BOLD: 800,
+      FULL_BOLD: 900
+    },
+    size: {
+      XSMALL: '1.2rem',
+      SMALL: '1.4rem',
+      MEDIUM: '1.6rem',
+      LARGE: '1.8rem',
+      XLARGE: '2.0rem',
+      XXLARGE: '2.8rem'
+    }
+  }
+}
+
+export default theme
